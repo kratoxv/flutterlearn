89884cb7507344a4f965bf2a3cd8019d8448a257
Jigar <prajapatijigar078@gmail.com>
2022-06-17 12:28:41 +0530

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int day = 10;
+    String name = "Welcome to $day days of flutter";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My First flutter App "),
+      ),
+      body: Center(
+        child: Container(
+          child: Text(" $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
